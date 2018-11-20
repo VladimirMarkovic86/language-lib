f615941b19fb701aabc9cac9498558a9bbb300a4
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-11-20 13:59:35 +0100

    Utils library project version was upgraded so its dependent projects change their versions also.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.1"
+(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.2"
   :description "Language library"
   :url "http://github.com/VladimirMarkovic86/language-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.2"]
-                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.1"]
+                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.2"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.2"]
                  [org.clojure/clojurescript "1.10.339"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"]
