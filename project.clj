219cab18b075b6d0d38dc43895ae53962bee48bf
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-02-06 14:19:18 +0100

    Task 38: Implemented calories calculator

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.7"
+(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.8"
   :description "Language library"
   :url "http://github.com/VladimirMarkovic86/language-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.4"]
-                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.4"]
+                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.5"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.7"]
                  [org.clojure/clojurescript "1.10.339"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.3"]
