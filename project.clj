9a92f5030f5ead943e64991ace803eff591d57fc
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-18 16:08:36 +0100

    Task63: Fixed optimization advanced to work with new code, compiling production version fixed

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.14"
+(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.15"
   :description "Language library"
   :url "http://github.com/VladimirMarkovic86/language-lib"
   :license {:name "Eclipse Public License"
@@ -6,9 +6,9 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.4"]
                  [org.clojars.vladimirmarkovic86/dao-lib "0.3.9"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.11"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.12"]
                  [org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.7"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.8"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.5"]
                  ]
 
