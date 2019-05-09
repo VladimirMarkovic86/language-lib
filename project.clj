48b2f7b3651a818c2abbccfef3008cf979aa105f
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-05-09 17:28:23 +0200

    Task 66: Implemented charts in svg-lib and used it in sample-client.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.23"
+(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.24"
   :description "Language library"
   :url "http://github.com/VladimirMarkovic86/language-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.8"]
-                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.16"]
+                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.17"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.18"]
                  [org.clojure/clojurescript "1.10.339"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.9"]
