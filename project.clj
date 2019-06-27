b6987798f4a356299892ae98fd57499d6a551b99
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-06-27 11:06:04 +0200

    Task 56: Implemented audio streaming in chat area.

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.30"
+(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.31"
   :description "Language library"
   :url "http://github.com/VladimirMarkovic86/language-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.8"]
-                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.22"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.22"]
+                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.23"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.23"]
                  [org.clojure/clojurescript "1.10.339"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.11"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.8"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.9"]
                  ]
 
   :min-lein-version "2.0.0"
