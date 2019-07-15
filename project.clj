b45f277fcb17c7ce1cd0ee1f23218fe56010b48d
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 18:17:36 +0200

    Task 88: Bug fix.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.36"
+(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.37"
   :description "Language library"
   :url "http://github.com/VladimirMarkovic86/language-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.12"]
-                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.28"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.28"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.13"]
+                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.29"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.29"]
                  [org.clojure/clojurescript "1.10.339"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.14"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.11"]
