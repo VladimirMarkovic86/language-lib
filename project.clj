a23347d971515cf048dfcf9337bdeaca103ededb
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:53:16 +0200

    Task 88: Removed README.md and LICENSE files from packed jar files.

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.34"
+(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.35"
   :description "Language library"
   :url "http://github.com/VladimirMarkovic86/language-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.10"]
-                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.26"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.26"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.11"]
+                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.27"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.27"]
                  [org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.12"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.9"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.13"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.10"]
                  ]
 
   :min-lein-version "2.0.0"
@@ -17,7 +17,9 @@
   :source-paths ["src/clj" "src/cljs"]
   :test-paths ["test/clj"]
   
-  :jar-exclusions [#"^public/"]
+  :jar-exclusions [#"^public/"
+                   #"README.md$"
+                   #"LICENSE$"]
 
   :plugins [[lein-cljsbuild  "1.1.7"]
             [lein-doo "0.1.11"]
