2f6b184eae975b29afee4cfcae4d7bd99c295562
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-08-01 13:17:43 +0200

    Task 95: Refactored the way routing works.

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.37"
+(defproject org.clojars.vladimirmarkovic86/language-lib "0.2.38"
   :description "Language library"
   :url "http://github.com/VladimirMarkovic86/language-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.13"]
-                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.29"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.29"]
+                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.30"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.30"]
                  [org.clojure/clojurescript "1.10.339"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.14"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.11"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.12"]
                  ]
 
   :min-lein-version "2.0.0"
